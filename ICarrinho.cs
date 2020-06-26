c6dc359d6f58e069d5a2d0137691074ae771e83d
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-26 09:45:26 -0300

    Estrutura da interface carrinho finalizada.

---
+++ ICarrinho.cs
@@ -0,0 +1,16 @@
+namespace Interfaces
+{
+    public interface ICarrinho
+    {
+         //CRUD
+         //Create
+         //Read
+         //Update
+         //Delete
+
+         void Add();
+         void Remove();
+         void Read();
+         void Update();
+    }
+}
--- Produto.cs
+++ Produto.cs
@@ -3,5 +3,18 @@ namespace Interfaces
     public class Produto
     {
         
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto(){
+
+        }
+        public Produto(int _cod, string _nome, float _preco){
+            this.Codigo = _cod;
+            this.Nome  = _nome;
+            this.Preco = _preco;
+        }
+
     }
 }
